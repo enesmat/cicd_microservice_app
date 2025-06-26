8ff2060440ee4f731f2a056c35576080ab3d2357
enesmat <enes.matzir.sali@skyered-education.de>
2025-06-26 16:42:59 +0200

    updated javafile

--- Aufgabe/abschlussprojekt_kukuk_mit_tests/backend/src/main/java/com/kukuk/app/Application.java
+++ Aufgabe/abschlussprojekt_kukuk_mit_tests/backend/src/main/java/com/kukuk/app/Application.java
@@ -1,25 +1,9 @@
 package com.kukuk.app;
 
-// Importiere benötigte Spring Boot-Klassen
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-// Markiert das Projekt als Spring Boot Anwendung + aktiviert automatische Konfiguration
-@SpringBootApplication
-
-// Einfacher REST-Controller direkt in der Main-Klasse
-@RestController
 public class Application {
 
     // Einstiegspunkt der Anwendung
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    // Einfacher HTTP-Endpunkt für Testzwecke (Jenkins)
-    @GetMapping("/")
-    public String start() {
-        return "Kukuk App läuft";
+        System.out.println("Hello Kukuk DevOps!");
     }
 }
